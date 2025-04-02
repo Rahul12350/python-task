5e8a931e7cd60a2eb1cc5c633586cebd90c1cf0c
Rahul12350 <rroy36632@gmail.com>
2025-04-02 20:19:45 +0530

    Added Task 1 and Task 2 Python scripts

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+
+num1=int(input("Enter your 1st number : "))
+num2=int(input("Enter your 2nd nubmer : "))
+print("Here is your answer :")
+print("Addition :",num1+num2)
+print("Subtraction :",num1-num2)
+print("Multiplication :",num1*num2)
+print("Division :", num1/num2)
---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+first_name=input("Enter your First name : ")
+last_name=input("Enter your last name : ")
+Fulname= first_name + " " +last_name
+print("Hello, " +Fulname+ "!  Welcome to the python program")
