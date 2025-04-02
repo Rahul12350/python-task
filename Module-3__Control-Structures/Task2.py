edf38210b5599811157d865c935bbf21746f505b
Rahul12350 <rroy36632@gmail.com>
2025-04-03 00:18:51 +0530

    Control-Structures in python

---
+++ Module-3__Control-Structures/Task2.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total = 0
+
+# Iterate over numbers from 1 to 50
+for i in range(1, 51):
+    total += i  # Add each number to the total
+
+# Display the result
+print(f"The sum of integers from 1 to 50 is: {total}")
